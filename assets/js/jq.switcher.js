02db9d28de885275ec9a5532436001725810313a
visioncan <visioncan@gmail.com>
2013-02-19 12:41:57 +0800

    first creat page

---
+++ assets/js/jq.switcher.js
@@ -0,0 +1,84 @@
+/************************  
+	switch plugin 
+************************/
+/*
+ * jQuery toggle Switch Plugin
+ * Author: visioncan
+ * date 2011-08-31
+ * Copyright:
+ * 
+ * @example: $.toggleSwitcher();
+ * @html: <input name="" type="switch" value="1" />
+ * @param Object  callback - callback function on switchs
+ * @return jQuery
+*/
+(function($){
+	var defaults = {
+		callback  : null,
+		statclass : ["off", "on"],
+		label     : ["OFF", "ON"]
+	};
+	$.toggleSwitcher = function(opt){
+		//$.log( $("input[type=switch]") );
+		var options = $.extend({}, defaults, opt);
+		$("input[type='switch']").each(function(i, n){
+			creatElement(n, options);
+		});
+	};
+	
+	$.fn.toggleSwitcher = function(opt){
+		var options = $.extend({}, defaults, opt);
+		creatElement(this.get(0), options);
+	};
+	
+	function switchHandler(e){
+		/*var Switcher = e.currentTarget,
+			BG       = e.currentTarget.getElementsByClassName("bg"),
+			Input    = e.currentTarget.children[0]*/
+		var options  = $(this).data("opt");
+		var Switcher = $(this),
+			BG       = $(this).children(".bg"),
+			Input    = $(this).children("input[type='hidden']");
+		if( Switcher.hasClass("on") ){
+			BG.stop().animate({ left: -42} , 180 , function(){
+				Input.val(0);
+				Switcher.removeClass("on").addClass("off");
+				if( options.callback ){
+					options.callback(0, Switcher);
+				}
+				Switcher = BG = Input = null;
+			});
+		}else{
+			BG.stop().animate({ left: 0} , 180 , function(){
+				Input.val(1);
+				Switcher.removeClass("off").addClass("on");
+				if( options.callback ){
+					options.callback(1, Switcher);
+				}
+				Switcher = BG = Input = null;
+			});
+		}
+	}
+	
+	function creatElement(target, options){
+		var wrapClass = options.statclass[target.value];
+		var wrap = $("<div/>",{
+			"class"  : "switches " + wrapClass
+		}).data("opt", options).click(switchHandler);
+		$(target).wrap(wrap);	
+		
+		var bg = $("<div/>",{
+			"class"  : "bg",
+			"html"   : '<span class="on">'+ options.label[1] +'</span><span class="off">'+ options.label[0] +'</span>'
+		}).insertAfter(target);
+		
+		var outter = $('<span class="lf"></span><span class="rt"></span>').insertAfter(bg);
+		
+		var input = $("<input/>",{
+			type  : "hidden",
+			name  : target.getAttribute("name"),
+			value : target.value
+		}).insertAfter(target);
+		$(target).remove();
+	}
+})(jQuery);
